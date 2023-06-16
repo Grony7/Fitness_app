e330748a799729333dfdfddd538f7cc554dd474f
Grony <gr0ny.0017@gmail.com>
2023-06-16 03:07:33 +0300

    Routes

--- server/app/exercise/log/exercise-log.controller.js
+++ server/app/exercise/log/exercise-log.controller.js
@@ -1,15 +1,12 @@
-import asyncHandler from 'express-async-handler';
+import asyncHandler from 'express-async-handler'
 
-
-
-import { prisma } from '../../prisma.js';
-
+import { prisma } from '../../prisma.js'
 
 // @desc    Create new exerciseLog
 // @route   POST /api/exercises/log/:exerciseId
 // @access  Private
 export const createNewExerciseLog = asyncHandler(async (req, res) => {
-	const exerciseId = +req.params.exerciseId
+	const exerciseId = +req.params.id
 
 	const exercise = await prisma.exercise.findUnique({
 		where: {
@@ -22,10 +19,14 @@ export const createNewExerciseLog = asyncHandler(async (req, res) => {
 		throw new Error('Exercise not found!')
 	}
 
-  const timesDefault = Array.from({ length: exercise.times }, () => ({
-    weight: 0,
-    repeat: 0,
-  }));
+	let timesDefault = []
+
+	for (let i = 0; i < exercise.times; i++) {
+		timesDefault.push({
+			weight: 0,
+			repeat: 0
+		})
+	}
 
 	const exerciseLog = await prisma.exerciseLog.create({
 		data: {
